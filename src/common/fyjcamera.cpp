8c1efdc20c1bbd1eef1452f3933293552d824d51
fuyajun01 <fuyajun1983cn@gmail.com>
2014-05-14 18:27:41 +0800

    update fyjcamera.cpp

--- src/common/fyjcamera.cpp
+++ src/common/fyjcamera.cpp
@@ -1,44 +1,87 @@
-#ifndef FYJCAMERA_H
-#define FYJCAMERA_H
+#include "fyjcamera.h"
 
-#include "fyjstd.h"
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/matrix_inverse.hpp>
-#include <glm/gtc/type_ptr.hpp>
+using namespace FYJ;
 
-namespace FYJ
+Camera::Camera()
 {
-	class FYJ_API Camera
-	{
-		public:
-			Camera();
-			virtual ~Camera();
-
-			void translate(float x, float y, float z);
-			void scale(float x, float y, float z);
-			void rotate(float angle, float x, float y, float z);
-			void setLookAt(float eyeX, float eyeY, float eyeZ, float centerX, float centerY, float centerZ, float upX, float upY, float upZ);
-			void setPerspective(float fovy, float aspect, float near, float far);
-			void setOrtho(float left, float right, float bottom, float top);
-			void reset();
-			void setLightPos(float x, float y, float z, float w);
-
-			void *vPtrForMVP();
-			void *vPtrForMV();
-			void *vPtrForProj();
-			void *vPtrForNM(); //normal matrix
-			void *vPtrForLightPos();
-
-		private:
-			glm::mat4 mMVMatrix;
-			glm::mat4 mResetValueForMV;
-			glm::mat4 mProjMatrix;
-			glm::mat3 mNormalMatrix;
-
-			//light position
-			glm::vec4 mLightPos;
-	};
 }
 
-#endif
+Camera::~Camera()
+{
+}
+
+void Camera::translate(float x, float y, float z)
+{
+	mMVMatrix = glm::translate(mMVMatrix, glm::vec3(x, y, z));
+	mNormalMatrix = glm::inverseTranspose(glm::mat3(mMVMatrix));
+}
+
+void Camera::scale(float x, float y, float z)
+{
+	mMVMatrix = glm::scale(mMVMatrix, glm::vec3(x , y, z));
+	mNormalMatrix = glm::inverseTranspose(glm::mat3(mMVMatrix));
+}
+
+void Camera::rotate(float angle, float x, float y, float z)
+{
+	mMVMatrix = glm::rotate(mMVMatrix, angle, glm::vec3(x, y, z));
+	mNormalMatrix = glm::inverseTranspose(glm::mat3(mMVMatrix));
+}
+
+void Camera::setLookAt(float eyeX, float eyeY, float eyeZ, float centerX, float centerY, float centerZ, float upX, float upY, float upZ)
+{
+	mMVMatrix = glm::lookAt(glm::vec3(eyeX, eyeY, eyeZ), glm::vec3(centerX, centerY, centerZ), glm::vec3(upX, upY, upZ));
+	mResetValueForMV = mMVMatrix;
+	mNormalMatrix = glm::inverseTranspose(glm::mat3(mMVMatrix));
+}
+
+void Camera::setPerspective(float fovy, float aspect, float near, float far)
+{
+	mProjMatrix = glm::perspective(fovy, aspect, near, far);
+}
+
+void Camera::setOrtho(float left, float right, float bottom, float top)
+{
+	mProjMatrix = glm::ortho(left, right, bottom, top);
+}
+
+void Camera::reset()
+{
+	mMVMatrix = mResetValueForMV;
+	mNormalMatrix = glm::inverseTranspose(glm::mat3(mMVMatrix));
+}
+
+void Camera::setLightPos(float x,float y,float z,float w)
+{
+	mLightPos = glm::vec4(x, y, z, w);
+}
+
+void *Camera::vPtrForMVP()
+{
+	glm::mat4 tempMatrix = mProjMatrix * mMVMatrix;
+	return glm::value_ptr(tempMatrix);
+}
+
+void *Camera::vPtrForMV()
+{
+	return glm::value_ptr(mMVMatrix);
+}
+
+void *Camera::vPtrForProj()
+{
+	return glm::value_ptr(mProjMatrix);
+}
+
+void *Camera::vPtrForNM()
+{
+	//MV矩阵左上角3X3子矩阵转置的逆
+	//glm::mat3 nm = glm::inverseTranspose(glm::mat3(mMVMatrix));
+	return glm::value_ptr(mNormalMatrix);
+}
+
+void *Camera::vPtrForLightPos()
+{
+	return glm::value_ptr(mLightPos);
+}
+
+
